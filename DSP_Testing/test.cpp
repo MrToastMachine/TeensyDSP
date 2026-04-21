0fc0cdc2be347d30509d9c8b9783b705184a991c
Alex OMahony <alex.o@ulysses.eco>
2026-04-20 21:55:07 -0700

    work on mapping to desired freq

--- DSP_Testing/test.cpp
+++ DSP_Testing/test.cpp
@@ -1,16 +1,34 @@
+#include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <complex>
 
 #define NUM_FREQS 49
 
-const float allPossibleFreqs[NUM_FREQS] = { 27.5 ,  29.14,  30.87,  32.7 ,  34.65,  36.71,  38.89,  41.2 ,
+// This starts at C1 (octave 1) and goes to A4 -----> ONLY A:440Hz TUNING
+const float allPossibleFreqs[NUM_FREQS] = {32.7 ,  34.65,  36.71,  38.89,  41.2 ,
         43.65,  46.25,  49.  ,  51.91,  55.  ,  58.27,  61.74,  65.41,
         69.3 ,  73.42,  77.78,  82.41,  87.31,  92.5 ,  98.  , 103.83,
        110.0  , 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81,
        174.61, 185.  , 196.  , 207.65, 220.  , 233.08, 246.94, 261.63,
        277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.  , 415.3 ,
        440.0  };
+
+const std::string NOTES[12] = {
+	"C",
+	"C#",
+	"D",
+	"D#",
+	"E",
+	"F",
+	"F#",
+	"G",
+	"G#",
+	"A",
+	"A#",
+	"B"
+};
+
 
 int findClosestIndex(const float *arr, uint8_t arrLen, float target){
     float res = arr[0];
@@ -47,8 +65,44 @@ int findClosestIndex(const float *arr, uint8_t arrLen, float target){
     // return res;
 }
 
+void getNearestNoteAndPrint(float note){
+	int index = findClosestIndex(allPossibleFreqs, NUM_FREQS, note);
+	float closest_note_actual = allPossibleFreqs[index];
+	int note_index = index % 12;
+
+	int note_octave = index / 12 + 1;
+
+	printf("Closest index in freq bank -> %d\n", index);
+
+	printf("Closest Frequency -> %f\n", closest_note_actual);
+
+	printf("The closest note is %s%d\n", NOTES[note_index].c_str(), note_octave);
+
+	// The next thing to do is check distance to nearest notes
+	//
+	// Plot nearest note in center
+	// halfway to one note lower on left
+	// halfway to one note higher on right
+	// map this range to screen (arbitrary)
+	
+	printf("\n");
+		
+	int higher_note_index = std::min(index + 1, NUM_FREQS - 1);
+	int lower_note_index = std::max(index - 1, 0);
+	
+	printf("One note higher >> %f\n", allPossibleFreqs[higher_note_index]);
+	printf("One note lower >> %f\n", allPossibleFreqs[lower_note_index]);
+
+
+	float halfway_to_higher_note = (allPossibleFreqs[higher_note_index] - closest_note_actual) / 2;
+	float halfway_to_lower_note = (closest_note_actual - allPossibleFreqs[lower_note_index]) / 2;
+
+	printf("Distance to halfway to up: %f\n", halfway_to_higher_note);
+	printf("Distance to halfway to down: %f\n", halfway_to_lower_note);
+}
+
 int main(int argc, char* argv[]){
-	printf("AssBegin\n");
+
 
 	if (argc < 2){
 		printf("not enough arguments\n");
@@ -56,14 +110,9 @@ int main(int argc, char* argv[]){
 	}
 
 	float test = std::atof(argv[1]);
-	
 	printf("Arg given -> %f\n", test);
+	getNearestNoteAndPrint(test);
 
-
-	int index = findClosestIndex(allPossibleFreqs, NUM_FREQS, test);
-	printf("Closest index in freq bank -> %d\n", index);
-
-	printf("Closest Frequency -> %f\n", allPossibleFreqs[index]);
 
 	return 0;	
 }
